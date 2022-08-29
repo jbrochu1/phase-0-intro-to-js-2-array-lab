6e24503f57541c8985114d4359208cd96ce7f376
Jason Brochu <jbrochu1@gmail.com>
2022-08-29 15:15:56 -0600

    commit tree d3ecfabdea6e52ad5318df0f108ed10765ebd9d3

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const catCopy = [...cats, name];
+    return catCopy;
+}
+
+function prependCat(name) {
+    const catCopy2 = [name, ...cats];
+    return catCopy2;
+
+}
+
+function removeLastCat() {
+    const catCopy2 = [...cats];
+    catCopy2.pop();
+    return catCopy2;
+}
+
+function removeFirstCat() {
+    const catCopy4 = [...cats];
+    catCopy4.shift();
+    return catCopy4;
+}
+
